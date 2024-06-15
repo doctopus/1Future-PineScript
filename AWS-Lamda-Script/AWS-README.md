e50121b6c4c7271439449b2d37cc4a1023461569
Doctopus <tapasapt@gmail.com>
2024-06-15 10:13:37 -0400

    Visual representation of time interval in notification

--- AWS-Lamda-Script/AWS-README.md
+++ AWS-Lamda-Script/AWS-README.md
@@ -9,26 +9,64 @@ Which then sends them as notifications in the Pushover application on phone.
 
 Create an AWS Lambda function with the code of lambda_function.py 
 
+### Create AWS Lambda Layer
 Create a layer of dependencies and upload them as a layer for the lambda function.
 
-```angular2html
-mkdir combined_layer
-cd combined_layer
+Follow the steps outlined in [Amazon](https://docs.aws.amazon.com/lambda/latest/dg/python-layers.html#python-layer-packaging) to create a library layer to add to Lambda:
+Folder structure should be as follows
+```
+layer
+├── 1-install.sh
+├── 2-package.sh
+├── create_layer
+│   └── pyvenv.cfg
+└── requirements.txt
+```
+requirements.txt
+```
+requests==2.32.3
+pytz==2024.1
+python-dotenv==1.0.1
+```
+1-install.sh
+```
+python3.12 -m venv create_layer
+source create_layer/bin/activate
+pip install -r requirements.txt
+```
+2-package.sh
+```
 mkdir python
-
-
-pip install requests python-dotenv -t python
-
-zip -r ../combined_layer.zip python
-cd ..
-
+cp -r create_layer/lib python/
+zip -r layer_content.zip python
 ```
 
-Upload the combined_layer.zip as a layer
+In the terminal
+`chmod 744 1-install.sh && chmod 744 2-package.sh`
+`./1-install.sh`
+`./2-package.sh`
 
+Upload the resulting layer_content.zip file
+
+- **Upload the Custom Layer**:
+    - Go to the AWS Lambda console.
+    - Navigate to **Layers** > **Create layer**.
+    - Upload `layer_content.zip`.
+    - Choose the compatible runtime (Python 3.12).
+- **Attach the Custom Layer to Your Function**:
+
+    - Go to your Lambda function (`AWS-TV`).
+    - In the **Layers** section, click **Add a layer**.
+    - Choose **Custom layers** and select your  layer.
+- **Update Your Function Code**:
+
+    - Update your function code to use the library as needed.
+
+### Add Environment Variables
 Add environment variables from the configuration menu of functions
 Go to your Lambda function.
 Under "Configuration" > "Environment variables," add PUSHOVER_USER_KEY and PUSHOVER_API_TOKEN.
 
-PUSHOVER_USER_KEY=your_user_key
-PUSHOVER_API_TOKEN=your_api_token
+PUSHOVER_USER_KEY=your_user_key    
+PUSHOVER_API_TOKEN=your_api_token    
+DYNAMODB_TABLE_NAME=your_dynamodb_table
--- AWS-Lamda-Script/lambda_function.py
+++ AWS-Lamda-Script/lambda_function.py
@@ -1,12 +1,13 @@
 import json
-import urllib.request
+import urllib.request #From layers: requests
 import urllib.parse
 import logging
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv #From layers: python-dotenv
 import boto3
-import pytz
+import pytz #From layers: pytz
 from datetime import datetime
+import math  # For calculating number of emojis
 
 # Initialize DynamoDB client
 dynamodb = boto3.client('dynamodb')
@@ -20,6 +21,8 @@ logger.setLevel(logging.INFO)
 # Retrieve the keys from environment variables
 PUSHOVER_USER_KEY = os.getenv('PUSHOVER_USER_KEY')
 PUSHOVER_API_TOKEN = os.getenv('PUSHOVER_API_TOKEN')
+
+visual_emoji = "♦️"
 
 def lambda_handler(event, context):
     logger.info(f"Event received: {json.dumps(event)}")
@@ -35,8 +38,8 @@ def lambda_handler(event, context):
             "sell": "🔴😡🔴"
         }
         word_map = {
-            "buy": '<font color="#00ff00">Long</font>',
-            "sell": '<font color="#ff0000">Short</font>'
+            "buy": '<font color="#00ff00">📈</font>',
+            "sell": '<font color="#ff0000">📉</font>'
         }
 
         if words:
@@ -56,14 +59,25 @@ def lambda_handler(event, context):
         time_interval = words[2].split('@')[1] if '@' in words[2] else ''
         alert_price = words[3].split('$')[1] if '$' in words [3] else ''
         alert_time_str = words[4].split('#')[1] if '#' in words [4] else ''
-        #atr_range = next((word.split(':')[1] for word in words if word.startswith('Range:')), '')
         atr_range = words[5].split(':')[1] if '*' in words[5] else ''
 
+
+        # Convert time_interval to seconds
+        if time_interval.endswith('S'):
+            time_interval_seconds = int(time_interval[:-1])
+        else:
+            time_interval_seconds = int(time_interval) * 60  # Interval without S is in minutes
+
+        # Format alert_price to two decimal places if it's not empty
+        if alert_price:
+            try:
+                alert_price = f"{float(alert_price):.2f}"
+            except ValueError:
+                alert_price = ''  # Handle the case where conversion fails
 
         #Convert alert_time to UTC
         try:
             alert_time = datetime.strptime(alert_time_str, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=pytz.utc)
-
         #Convert to EST
             est = pytz.timezone('US/Eastern')
             alert_time_est = alert_time.astimezone(est)
@@ -73,14 +87,8 @@ def lambda_handler(event, context):
             alert_time_est_timestamp = None
             alert_time_est_formatted = alert_time_str
 
-        # Convert time_interval to seconds
-        if time_interval.endswith('S'):
-            time_interval_seconds = int(time_interval[:-1])
-        else:
-            time_interval_seconds = int(time_interval) * 60  # Interval without S is in minutes
 
-
-        # Construct the item to store in DynamoDB
+        # DynamoDB: Construct the item to store in DynamoDB
         item = {
         'alert_id': {'S': context.aws_request_id},
         'ticker_name': {'S': ticker_name},
@@ -90,15 +98,45 @@ def lambda_handler(event, context):
         'time_est': {'S': str(alert_time_est)},
         'atr_range': {'S': atr_range}
         }
-
         # Put item into DynamoDB table
         response = dynamodb.put_item(
             TableName=os.getenv('DYNAMODB_TABLE_NAME'),
             Item=item
         )
 
-        # Prepare the message for Pushover
-        message = f"{words[0]} {words[1]} @{words[2].split('@')[1]} ${words[3].split('$')[1]} #{alert_time_est_formatted}"
+
+        # PushOver: Prepare the message for Pushover
+        # Convert time_interval to an integer and ignore 'S'
+        if 'S' in time_interval:
+            time_interval_value = 0
+        else:
+            try:
+                time_interval_value = int(time_interval)
+            except ValueError:
+                time_interval_value = 0  # Default to 0 if not a valid integer
+
+        # Normalize the interval value to determine the number of emojis
+        # Define normalization parameters
+        min_interval = 1
+        max_interval = 15
+        min_visual_units = 1
+        max_visual_units = 5 # We can limit the number of emojis to a maximum of 5 to avoid excessive length
+
+        if min_interval <= time_interval_value <= max_interval:
+            visual_units = math.ceil(
+                (time_interval_value - min_interval) / (max_interval - min_interval) * (max_visual_units - min_visual_units) + min_visual_units
+            )
+        else:
+            visual_units = 0  # Set to 0 if the value is out of range or contains 'S'
+        #visual_units = min(max_visual_units, time_interval_value)
+
+        # Create a visual representation of the time interval
+        time_interval_visual = visual_emoji * visual_units
+
+        message = f"{words[0]} @{time_interval} {time_interval_visual} ${alert_price} #{alert_time_est_formatted}"
+        #message = f"{words[0]} {words[1]} @{words[2].split('@')[1]} {time_interval_visual} ${words[3].split('$')[1]} #{alert_time_est_formatted}"
+        #message = f"{words[0]} {words[1]} @{time_interval} {time_interval_visual} ${alert_price} #{alert_time_est_formatted}"
+
 
         #PushOver Alert Message
         url = "https://api.pushover.net/1/messages.json"
@@ -108,7 +146,7 @@ def lambda_handler(event, context):
             "message": message,
             "title": title,
             "html": 1,
-            "ttl": 10800 # 30 Minute Notification Expiry
+            "ttl": 21600 # 1 Hour Notification Expiry
         }
 
         # Encode the data
